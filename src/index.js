1c5967db1d288bb97f41e6bde14ae206339389d4
AlmuLloret <almudenalloret@gmail.com>
2023-04-10 09:51:13 +0200

    update 4.6

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path"); // para crear las rutas not found - no lo hemos usado
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.set("view engine", "ejs"); // para la config del motor de plantillas
 
 // init express aplication
 const serverPort = 4000;
@@ -16,6 +18,7 @@ const mysql = require("mysql2/promise");
 
 let connection; // Aquí almacenaremos la conexión a la base de datos
 
+// conexión a mysql
 mysql
   .createConnection({
     host: "localhost",
@@ -40,8 +43,10 @@ mysql
     console.error("Error de configuración: " + err.stack);
   });
 
+// filtra por género + ordena por nombre
+
 server.get("/movies", (req, res) => {
-  console.log("Pidiendo a la base de datos información de los empleados.");
+  console.log("Pidiendo a la base de datos información de netflix");
   const genreFilterParam = req.query.genre;
   const sortFilterParam = req.query.sort;
   if (genreFilterParam === "") {
@@ -52,14 +57,8 @@ server.get("/movies", (req, res) => {
         }`
       )
       .then(([results, fields]) => {
-        console.log("Información recuperada:");
-        results.forEach((result) => {
-          console.log(result);
-        });
-        res.json({
-          success: true,
-          movies: results,
-        });
+        results.forEach((result) => console.log(result));
+        res.json({ success: true, movies: results });
       })
       .catch((err) => {
         throw err;
@@ -67,20 +66,14 @@ server.get("/movies", (req, res) => {
   } else {
     connection
       .query(
-        `SELECT * from movies WHERE gender = ? ORDER BY title  ${
+        `SELECT * from movies WHERE genre = ? ORDER BY title  ${
           sortFilterParam === "asc" ? "asc" : "desc"
         }`,
         [genreFilterParam]
       )
       .then(([results, fields]) => {
-        console.log("Información recuperada:");
-        results.forEach((result) => {
-          console.log(result);
-        });
-        res.json({
-          success: true,
-          movies: results,
-        });
+        results.forEach((result) => console.log(result));
+        res.json({ success: true, movies: results });
       })
       .catch((err) => {
         throw err;
@@ -88,23 +81,22 @@ server.get("/movies", (req, res) => {
   }
 });
 
+// login
+
 server.post("/login", (req, res) => {
   console.log(req.body);
   const mailInput = req.body.email;
   const passwordInput = req.body.password;
 
   connection
-    .query(`SELECT idUsers from users WHERE email=? AND password=?`, [
+    .query(`SELECT * from users WHERE email=? AND password=?`, [
       mailInput,
       passwordInput,
     ])
     .then(([results, fields]) => {
-      console.log(result);
-      if (idUsers) {
-        res.json({
-          success: true,
-          userId: "id_de_la_usuaria_encontrada",
-        });
+      console.log(results);
+      if (results.length > 0) {
+        res.json({ success: true, userId: results[0].idUsers });
       } else {
         res.json({
           success: false,
@@ -116,3 +108,38 @@ server.post("/login", (req, res) => {
       throw err;
     });
 });
+
+// motor de plantillas - siempre antes de los servidores estáticos
+
+server.get("/movie/:movieId", (req, res) => {
+  const foundMovie = req.params.movieId;
+  console.log(foundMovie);
+  const sql = "SELECT  * FROM movies WHERE id = ?";
+
+  connection
+    .query(sql, [foundMovie])
+    .then(([results, fields]) => {
+      res.render("movie", results[0]);
+    })
+
+    .catch((err) => {
+      throw err;
+    });
+});
+
+// servidor de estáticos - la dirección debe ser relativa a la raiz del fichero, no al fichero index
+
+const staticServerPathWeb = "./src/public-react"; // relative to the root of the project
+server.use(express.static(staticServerPathWeb));
+
+const staticImagesPathWeb = "./src/public-movies-images";
+server.use(express.static(staticImagesPathWeb));
+
+// estilos motor de plantillas
+const staticCssPathWeb = "./src/public-movies-css";
+server.use(express.static(staticCssPathWeb));
+
+// En el front:
+// http://localhost:3000 - Modo desarrollo
+// http://localhost:4000 - Modo producción
+// desarrollo = > producción npm run publish-react
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -2,10 +2,13 @@
 
 const getMoviesFromApi = (params) => {
   console.log(params);
-  return fetch(`//localhost:4000/movies/?genre=${params.genre}`, {
-    method: "GET",
-    headers: { "content-type": "application/json" },
-  })
+  return fetch(
+    `//localhost:4000/movies/?genre=${params.genre}&sort=${params.sort}`,
+    {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    }
+  )
     .then((response) => response.json())
     .then((data) => {
       return data;
